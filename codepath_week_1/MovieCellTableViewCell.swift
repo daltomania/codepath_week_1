33b672bddc8a5e3424d6d0c2ba85f0b6fa30ecf1
Will Dalton <wdalton@tumblr.com>
2015-08-31 15:42:31 -0700

    custom cells

---
+++ codepath_week_1/MovieCellTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCellTableViewCell.swift
+//  codepath_week_1
+//
+//  Created by Will Dalton on 8/31/15.
+//  Copyright (c) 2015 daltomania. All rights reserved.
+//
+
+import UIKit
+
+class MovieCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- codepath_week_1/MoviesViewController.swift
+++ codepath_week_1/MoviesViewController.swift
@@ -46,9 +46,10 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! UITableViewCell
+        var cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCellTableViewCell
         let movie = movies![indexPath.row]
-        cell.textLabel?.text = movie["title"] as? String
+        cell.titleLabel.text = movie["title"] as? String
+        cell.synopsisLabel.text = movie["synopsis"] as? String
         
         return cell
     }
